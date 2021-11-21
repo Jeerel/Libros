28950cb96793d93c54ba0e02f6d4dac291bab86e
Jeerel <zhatwo@gmail.com>
2021-11-20 21:18:40 -0600

    make it better

--- src/pages/Pruebas.js
+++ src/pages/Pruebas.js
@@ -1,44 +1,210 @@
 import React, { Component } from 'react';
-import ReactExport from 'react-export-excel';
+import { CSVLink } from 'react-csv'
 
-const ExcelFile = ReactExport.ExcelFile;
-const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+/*
+import ExportExcel from 'react-export-excel';
+const ExcelFile = ExportExcel.ExcelFile;
+const ExcelSheet = ExportExcel.ExcelSheet;
+const ExcelColumn = ExportExcel.ExcelColumn;
+*/
 
-const multiDataSet = [
+/*const multiDataSet = [
     {
         columns: [
-            { value: "Name", widthPx: 50 }, // width in pixels
-            { value: "Salary", widthCh: 20 }, // width in charachters
-            { value: "Sex", widthPx: 60, widthCh: 20 }, // will check for width in pixels first
+            { title: "Headings", width: { wpx: 80 } },//pixels width 
+            { title: "Text Style", width: { wch: 40 } },//char width 
+            { title: "Colors", width: { wpx: 90 } },
         ],
         data: [
-            ["Johnson", 30000, "Male"],
-            ["Monika", 355000, "Female"],
-            ["Konstantina", 20000, "Female"],
-            ["John", 250000, "Male"],
-            ["Josef", 450500, "Male"],
-        ],
-    },
+            [
+                { value: "H1", style: { font: { sz: "24", bold: true } } },
+                { value: "Bold", style: { font: { bold: true } } },
+                { value: "Red", style: { fill: { patternType: "solid", fgColor: { rgb: "FFFF0000" } } } },
+            ],
+            [
+                { value: "H2", style: { font: { sz: "18", bold: true } } },
+                { value: "underline", style: { font: { underline: true } } },
+                { value: "Blue", style: { fill: { patternType: "solid", fgColor: { rgb: "FF0000FF" } } } },
+            ]
+        ]
+    }
+];*/
+
+/*const ciudades = [
     {
-        xSteps: 1, // Will start putting cell with 1 empty cell on left most
-        ySteps: 5, //will put space of 5 rows,
-        columns: ["Name", "Department"],
-        data: [
-            ["Johnson", "Finance"],
-            ["Monika", "IT"],
-            ["Konstantina", "IT Billing"],
-            ["John", "HR"],
-            ["Josef", "Testing"],
+        columns: [
+            { title: "Ciudad", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } },
+            { title: "Poblacion", style: { font: { sz: "18", bold: true } }, width: { wch: 30 } },
+            { title: "Entidad", style: { font: { sz: "18", bold: true } }, width: { wpx: 100 } },
+            { title: "Pais", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } },
         ],
+
+        data: [
+
+            {
+                value: "Mexico"
+            },
+            {
+                value: "España"
+            },
+            {
+                value: "Region"
+            },
+            {
+                value: "Religion"
+            }
+
+            {
+                 ciudad: "Ciudad de México",
+                 poblacion: 7685282,
+                 entidad: "Ciudad de Mexico",
+                 pais: "México"
+             },
+             {
+                 ciudad: "Ecatepec",
+                 poblacion: 23213,
+                 entidad: "Edo. de Mex",
+                 pais: "México"
+             },
+             {
+                 ciudad: "Tijuana",
+                 poblacion: 12342,
+                 entidad: "Baja California",
+                 pais: "México"
+             },
+             {
+                 ciudad: "Puebla",
+                 poblacion: 948283,
+                 entidad: "Puebla",
+                 pais: "México"
+             }
+
+        ]
+    }
+
+]*/
+
+const headers = [
+    { label: "*Unique vendor identifier", key: "Uniquevendoridentifier" },
+    { label: "*Author", key: "Author" },
+    { label: "*Title", key: "Title" },
+    { label: "ISBN - hardcover", key: "ISBNhardcover" },
+    { label: "ISBN - paperback", key: "ISBNpaperback" },
+    { label: "*Place of publication", key: "Placeofpublication" },
+    { label: "*Publisher", key: "Publisher" },
+    { label: "*Date of publication", key: "Dateofpublication" },
+    { label: "*Physical description", key: "Physicaldescription" },
+    { label: "*Language", key: "Language" },
+    { label: "*ma (yymmdd)", key: "ma" },
+    { label: "*US$ (no decimal format)", key: "US" },
+    { label: "*US shipping$ (no decimal format)", key: "USshipping" },
+    { label: "Net amount $ (no decimal format)", key: "Netamount" },
+    { label: "*Invoice number", key: "Invoicenumber" },
+    { label: "*Number of copies (default is 1)", key: "Numberofcopies" },
+    { label: "*Vendor code", key: "Vendorcode" },
+    { label: "*Fund code", key: "Fundcode" },
+    { label: "*Location (two letter location code from order)", key: "Location" },
+
+];
+
+//MARC 001, 035	100$a	245$a	20$a	20$a	260$a	260$b	260$c	300		980$a	980$b	980$c	980$e	980$f	980$g	981$a	981$b	981$c
+/*Uniquevendoridentifier: , 
+Author:, 
+Title:, 
+ISBNhardcover:, 
+ISBNpaperback:, 
+Placeofpublication:, 
+Publisher:, 
+Dateofpublication:, 
+Physicaldescription:, 
+Language:, 
+ma:, 
+US:, 
+USshipping:, 
+Netamount:, 
+Invoicenumber:, 
+Numberofcopies:, 
+Vendorcode:, 
+Fundcode:, 
+Location:*/
+
+const libro = {
+    Uniquevendoridentifier: "mjse",
+    Author: "Marinis, Natalia de… [et al.] ; coordinador: Yerko Castro Neira",
+    Title: "Antropología de la violencia. Miradas etnográficas y posicionamientos críticos",
+    ISBNhardcover: "",
+    ISBNpaperback: "9786075256337",
+    Placeofpublication: "Puebla",
+    Publisher: "Benemérita Universidad Autónoma de Puebla, Dirección General de Publicaciones",
+    Dateofpublication: "2019",
+    Physicaldescription: "",
+    Language: "",
+    ma: "200304",
+    US: "1750",
+    USshipping: "",
+    Netamount: "1750",
+    Invoicenumber: "8592",
+    Numberofcopies: "1",
+    Vendorcode: "mjse",
+    Fundcode: "mexia",
+    Location: "ma"
+}
+
+const data = [
+    {
+        Uniquevendoridentifier: "MARC 001, 035",
+        Author: "100$a",
+        Title: "245$a",
+        ISBNhardcover: "20$a",
+        ISBNpaperback: "20$a",
+        Placeofpublication: "260$a",
+        Publisher: "260$b",
+        Dateofpublication: "260$c",
+        Physicaldescription: "300",
+        Language: "",
+        ma: "980$a",
+        US: "980$b",
+        USshipping: "980$c",
+        Netamount: "980$e",
+        Invoicenumber: "980$f",
+        Numberofcopies: "980$g",
+        Vendorcode: "981$a",
+        Fundcode: "981$b",
+        Location: "981$c"
     },
+
+    {
+        Uniquevendoridentifier: libro.Uniquevendoridentifier,
+        Author: libro.Author,
+        Title: libro.Title,
+        ISBNhardcover: libro.ISBNhardcover,
+        ISBNpaperback: libro.ISBNpaperback,
+        Placeofpublication: libro.Placeofpublication,
+        Publisher: libro.Publisher,
+        Dateofpublication: libro.Dateofpublication,
+        Physicaldescription: libro.Physicaldescription,
+        Language: libro.Language,
+        ma: libro.ma,
+        US: libro.US,
+        USshipping: libro.USshipping,
+        Netamount: libro.Netamount,
+        Invoicenumber: libro.Invoicenumber,
+        Numberofcopies: libro.Numberofcopies,
+        Vendorcode: libro.Vendorcode,
+        Fundcode: libro.Fundcode,
+        Location: libro.Location
+    }
+
 ];
 
 class Pruebas extends Component {
     render() {
         return (
-            <ExcelFile>
-                <ExcelSheet dataSet={multiDataSet} name="Organization" />
-            </ExcelFile>
+            <div>
+                <CSVLink data={data} headers={headers} >
+                    Download me
+                </CSVLink>;
+            </div>
         );
     }
 }
